da3171529d8853e69de32412532abdcd694f1ee0
Sejuty <tafannumnishat00@gmail.com>
2023-02-19 03:42:45 +0600

    ,

--- 19-Unreal Webcam Fun/app.js
+++ 19-Unreal Webcam Fun/app.js
@@ -165,32 +165,81 @@ captureMediaStream()
     console.error(e);
   });
 
-
 function paintCanvas() {
   return setInterval(() => {
     ctx.drawImage(video, 0, 0, width, height);
+    let pixels = ctx.getImageData(0, 0, width, height);
+    // pixels = redEffect(pixels)
+    // pixels = rgbSplit(pixels);
+    // ctx.globalAlpha = 0.05;
+    pixels = greenScreen(pixels);
+    // console.log(pixels)
+    ctx.putImageData(pixels, 0, 0);
   }, 16);
-
-
 }
 
-function takePhoto() {
+function redEffect(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i + 0] = pixels.data[i + 0] + 100; // RED
+    pixels.data[i + 1] = pixels.data[i + 1] - 50; // GREEN
+    pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // Blue
+  }
+  return pixels;
+}
+function rgbSplit(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i - 450] = pixels.data[i + 0]; // RED
+    pixels.data[i + 500] = pixels.data[i + 1]; // GREEN
+    pixels.data[i - 550] = pixels.data[i + 2]; // Blue
+  }
+  return pixels;
+}
 
-    //played the sound
+function greenScreen(pixels) {
+  const levels = {};
+
+  document.querySelectorAll('.rgb input').forEach((input) => {
+    levels[input.name] = input.value;
+  });
+
+  for (i = 0; i < pixels.data.length; i = i + 4) {
+    red = pixels.data[i + 0];
+    // console.log(red)
+    green = pixels.data[i + 1];
+    blue = pixels.data[i + 2];
+    alpha = pixels.data[i + 3];
+
+    if (red >= levels.rmin
+      && green >= levels.gmin
+      && blue >= levels.bmin
+      && red <= levels.rmax
+      && green <= levels.gmax
+      && blue <= levels.bmax) {
+      // take it out!
+      pixels.data[i + 3] = 0;
+    }
+    console.log(red)
+  }
+
+  return pixels;
+}
+
+
+function takePhoto() {
+  //played the sound
   snap.currentTime = 0;
   snap.play();
 
   //take the data out of the canvas
-  const data = canvas.toDataURL('image/jpeg')
-  console.log(data)
+  const data = canvas.toDataURL("image/jpeg");
+  // console.log(data);
 
-  const link = document.createElement('a')
-  link.href = data
-  link.setAttribute('download','pic')
-//   link.textContent = 'Download Image'
-link.innerHTML=`<img src="${data}" alt="pic"/>`
-  strip.insertBefore(link,strip.firstChild)
-
+  const link = document.createElement("a");
+  link.href = data;
+  link.setAttribute("download", "pic");
+  //   link.textContent = 'Download Image'
+  link.innerHTML = `<img src="${data}" alt="pic"/>`;
+  strip.insertBefore(link, strip.firstChild);
 }
 
 video.addEventListener("canplay", paintCanvas);
