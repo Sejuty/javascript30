6525ac5a766eaac4da12d694a4b503c7f769a114
Sejuty <tafannumnishat00@gmail.com>
2023-02-20 18:25:51 +0600

    done

--- 21-Geolocation based Speedometer and Compass/app.js
+++ 21-Geolocation based Speedometer and Compass/app.js
@@ -1,6 +1,19 @@
 const arrow = document.querySelector(".arrow");
+const latitude = document.querySelector(".latitude");
+const longitude = document.querySelector(".longitude");
 const speed = document.querySelector(".speed-value");
 
-navigator.geolocation.watchPosition((data)=>{
-    console.log(data)
-})
+navigator.geolocation.watchPosition(
+  (data) => {
+    console.log(data);
+    const mySpeed = data.coords.speed;
+    speed.textContent = mySpeed;
+    if (mySpeed === null) speed.textContent = "0";
+    arrow.style.transform = `rotate(${data.coords.heading}deg)`;
+    latitude.textContent = `Latitude : ${data.coords.latitude}`
+    longitude.textContent =`Longitude : ${data.coords.longitude}`
+  },
+  (err) => {
+    console.error(err);
+  }
+);
